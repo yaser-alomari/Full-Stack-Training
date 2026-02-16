a661a1e041e7372f0f70ac519c6c7140263d93d3
ahmadalomari98@gmail.com <your-email@example.com>
2026-02-17 00:30:30 +0300

    WebApp2

---
+++ WebApplication2/WebApplication2/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+//using MyWebApi.Models;
+using WebApplication2.Models;
+
+namespace MyWebApi.Controllers
+{
+    
+    public class UserController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            User user = new User
+            {
+                Id = 1,
+                Name = "ahmad",
+                Email = "ahmad@gmail.com"
+            };
+                
+                
+
+            return View(user);
+        }
+
+        
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace WebApplication2.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public User() { }
+
+    }
+}
