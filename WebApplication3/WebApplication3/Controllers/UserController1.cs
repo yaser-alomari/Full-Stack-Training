aa0acea643dc77e92cb8335959b6f8efb9c5ba35
ahmadalomari98@gmail.com <your-email@example.com>
2026-02-20 01:36:40 +0300

    Sam

---
+++ WebApplication3/WebApplication3/Controllers/UserController1.cs
@@ -0,0 +1,52 @@
+﻿using AspNetCoreGeneratedDocument;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+using WebApplication3.Models.classes;
+
+namespace MyWebApi.Controllers
+{
+    public class UserController : Controller
+    {
+        private static List<User> _users = new List<User> 
+        {
+            new User {Id = "1",Name = "Ahmad",Email="Ahmad@gmail.com" },
+            new User {Id = "2",Name = "Yasser",Email="Yasser@gmail.com" },
+        };
+        [HttpGet]
+        public IActionResult Index() 
+        {
+            return RedirectToAction(nameof(UserInfo));
+        }
+
+        // GET: Display Form
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View( "UserCreate/Index"); // هاد بيرجع Index.cshtml
+        }
+        [HttpGet]
+        public IActionResult UserInfo() 
+        {
+            return View("UserInfo/Index",_users);
+        }
+
+        // POST: Receive Form Data
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                // مؤقتًا نخزن رسالة في TempData
+                TempData["Message"] = "User created successfully!";
+                return RedirectToAction("Success");
+            }
+
+            user.Id = (_users.Count +1).ToString();
+            _users.Add(user);
+           
+            return RedirectToAction(nameof(UserInfo));
+        }
+
+       
+    }
+}
---
+++ WebApplication3/WebApplication3/Models/classes/User.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication3.Models.classes
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public User() { }
+    }
+}
